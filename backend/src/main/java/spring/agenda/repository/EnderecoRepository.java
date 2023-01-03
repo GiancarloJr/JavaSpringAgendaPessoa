145b97660bf946a15bed269a5132d9892711dc95
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2023-01-02 23:07:01 -0300

    Repository Pessoa and Endereco

--- backend/src/main/java/spring/agenda/repository/EnderecoRepository.java
+++ backend/src/main/java/spring/agenda/repository/EnderecoRepository.java
@@ -1,7 +1,9 @@
 package spring.agenda.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import spring.agenda.domain.Endereco;
 
 @Repository
-public interface EnderecoRepository {
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 }
--- backend/src/main/java/spring/agenda/repository/PessoaRepository.java
+++ backend/src/main/java/spring/agenda/repository/PessoaRepository.java
@@ -1,7 +1,9 @@
 package spring.agenda.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import spring.agenda.domain.Pessoa;
 
 @Repository
-public interface PessoaRepository {
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 }
