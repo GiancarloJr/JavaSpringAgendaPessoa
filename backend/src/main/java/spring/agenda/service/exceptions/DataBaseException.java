3c307cb7435ddae10b2e6e9e834407ea69bdad0a
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2023-01-02 23:05:41 -0300

    Exceptions Service

---
+++ backend/src/main/java/spring/agenda/service/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package spring.agenda.service.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
