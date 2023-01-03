1ec53642f20d4e385bf5594b5b50c5fff3c8f072
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2023-01-02 23:06:24 -0300

    DTO's

--- backend/src/main/java/spring/agenda/dto/EnderecoDTO.java
+++ backend/src/main/java/spring/agenda/dto/EnderecoDTO.java
@@ -1,4 +1,93 @@
 package spring.agenda.dto;
 
+import spring.agenda.domain.Endereco;
+import spring.agenda.domain.Pessoa;
+
+import javax.persistence.*;
+import java.util.Objects;
+
 public class EnderecoDTO {
+
+    private Long idEndereco;
+
+    private String logradouro;
+
+    private String cep;
+
+    private Integer numero;
+
+    private Pessoa pessoa;
+
+    public EnderecoDTO(){
+
+    }
+
+    public EnderecoDTO(Long idEndereco, String logradouro, String cep, Integer numero, Pessoa pessoa) {
+        this.idEndereco = idEndereco;
+        this.logradouro = logradouro;
+        this.cep = cep;
+        this.numero = numero;
+        this.pessoa = pessoa;
+    }
+
+    public EnderecoDTO(Endereco entity) {
+        this.idEndereco = entity.getIdEndereco();
+        this.logradouro = entity.getLogradouro();
+        this.cep = entity.getCep();
+        this.numero = entity.getNumero();
+        this.pessoa = entity.getPessoa();
+    }
+
+    public Long getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(Long idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EnderecoDTO that = (EnderecoDTO) o;
+        return idEndereco.equals(that.idEndereco) && Objects.equals(logradouro, that.logradouro) && Objects.equals(cep, that.cep) && Objects.equals(numero, that.numero) && Objects.equals(pessoa, that.pessoa);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idEndereco, logradouro, cep, numero, pessoa);
+    }
 }
--- backend/src/main/java/spring/agenda/dto/PessoaDTO.java
+++ backend/src/main/java/spring/agenda/dto/PessoaDTO.java
@@ -1,7 +1,84 @@
 package spring.agenda.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import spring.agenda.domain.Endereco;
+import spring.agenda.domain.Pessoa;
+
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 public class PessoaDTO {
-    
+
+    private Long idPessoa;
+
+    private String nomePessoa;
+
+    private LocalDateTime dataNascimento;
+
+    @JsonIgnore
+    private List<Endereco> endereco;
+
+    public PessoaDTO(){
+
+    }
+
+    public PessoaDTO(Long idPessoa, String nomePessoa, LocalDateTime dataNascimento, List<Endereco> endereco) {
+        this.idPessoa = idPessoa;
+        this.nomePessoa = nomePessoa;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+    }
+
+    public PessoaDTO(Pessoa entity) {
+        this.idPessoa = entity.getIdPessoa();
+        this.nomePessoa = entity.getNomePessoa();
+        this.dataNascimento = entity.getDataNascimento();
+        this.endereco = entity.getEndereco();
+    }
+
+    public Long getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(Long idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public String getNomePessoa() {
+        return nomePessoa;
+    }
+
+    public void setNomePessoa(String nomePessoa) {
+        this.nomePessoa = nomePessoa;
+    }
+
+    public LocalDateTime getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDateTime dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public List<Endereco> getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(List<Endereco> endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PessoaDTO pessoaDTO = (PessoaDTO) o;
+        return idPessoa.equals(pessoaDTO.idPessoa) && Objects.equals(nomePessoa, pessoaDTO.nomePessoa) && Objects.equals(dataNascimento, pessoaDTO.dataNascimento) && Objects.equals(endereco, pessoaDTO.endereco);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idPessoa, nomePessoa, dataNascimento, endereco);
+    }
 }
