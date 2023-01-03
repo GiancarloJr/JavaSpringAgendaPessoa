3c0ef6548064db633c7f36fc6030bdff70680ed8
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2023-01-02 23:06:11 -0300

    PessoaResource

--- backend/src/main/java/spring/agenda/resource/PessoaResource.java
+++ backend/src/main/java/spring/agenda/resource/PessoaResource.java
@@ -1,9 +1,50 @@
 package spring.agenda.resource;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import spring.agenda.dto.PessoaDTO;
+import spring.agenda.service.PessoaService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaResource {
+
+    @Autowired
+    private PessoaService pessoaService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PessoaDTO> buscarPessoaPorId(@PathVariable Long id) {
+        return ResponseEntity.ok().body(pessoaService.findByID(id));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<PessoaDTO>> buscarTodasPessoasSemMostrarContatos() {
+        return ResponseEntity.ok().body(pessoaService.buscarPessoasSemMostrarContatos());
+    }
+
+    @GetMapping("/allcompleto")
+    public ResponseEntity<List<PessoaDTO>> buscarTodasPessoasMostrarContatos() {
+        return ResponseEntity.ok().body(pessoaService.buscarPessoasMostrarContatos());
+    }
+
+
+    @PostMapping("/")
+    public ResponseEntity<PessoaDTO> adicionarPessoa(@Valid @RequestBody PessoaDTO pessoaDTO) {
+        return ResponseEntity.ok().body(pessoaService.salvarPessoa(pessoaDTO));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<PessoaDTO> atualizarPessoa(@PathVariable Long id, @Valid @RequestBody PessoaDTO pessoaDTO) {
+        return ResponseEntity.ok().body(pessoaService.atualizarPessoa(id, pessoaDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPessoa(@PathVariable("id") Long id) {
+        pessoaService.deletarPessoa(id);
+        return ResponseEntity.noContent().build();
+    }
 }
