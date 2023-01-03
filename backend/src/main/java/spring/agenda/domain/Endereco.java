2adccee141c511db6fb3d72b9c9a65649891471f
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2023-01-02 23:06:35 -0300

    Endereco Entity

--- backend/src/main/java/spring/agenda/domain/Endereco.java
+++ backend/src/main/java/spring/agenda/domain/Endereco.java
@@ -1,6 +1,7 @@
 package spring.agenda.domain;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "ENDERECOS")
@@ -20,8 +21,73 @@ public class Endereco {
     @Column(name = "END_NUMERO")
     private Integer numero;
 
+
     @ManyToOne
     @JoinColumn(name = "pessoa_id", referencedColumnName = "pes_id", nullable = false)
     private Pessoa pessoa;
 
+    public Endereco(){
+
+    }
+
+    public Endereco(Long idEndereco, String logradouro, String cep, Integer numero, Pessoa pessoa) {
+        this.idEndereco = idEndereco;
+        this.logradouro = logradouro;
+        this.cep = cep;
+        this.numero = numero;
+        this.pessoa = pessoa;
+    }
+
+    public Long getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(Long idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Endereco endereco = (Endereco) o;
+        return idEndereco.equals(endereco.idEndereco) && Objects.equals(logradouro, endereco.logradouro) && Objects.equals(cep, endereco.cep) && Objects.equals(numero, endereco.numero) && Objects.equals(pessoa, endereco.pessoa);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idEndereco, logradouro, cep, numero, pessoa);
+    }
 }
