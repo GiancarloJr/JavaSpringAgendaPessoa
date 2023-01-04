a6c823f4e27bb472ea4407a67570a77fc2f9f087
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2023-01-04 20:56:45 -0300

    Entity for Tests Service

---
+++ backend/src/test/java/spring/agenda/EntitiesForTests.java
@@ -0,0 +1,36 @@
+package spring.agenda;
+
+import spring.agenda.domain.Endereco;
+import spring.agenda.domain.Pessoa;
+import spring.agenda.dto.EnderecoDTO;
+import spring.agenda.dto.PessoaDTO;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EntitiesForTests {
+
+    public static Pessoa createPessoa(){
+        Pessoa pessoa = new Pessoa(1L,"Giancarlo",LocalDate.parse("03/12/1992", DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        pessoa.getEndereco().add(new Endereco(1L,"Casa","teste",1,"Palmas",pessoa));
+        return pessoa;
+    }
+
+    public static PessoaDTO createPessoaDTO(){
+        List<EnderecoDTO> enderecoList = new ArrayList<>();
+        enderecoList.add(createEnderecoDTO());
+        return new PessoaDTO(createPessoa());
+    }
+
+    public static Endereco createEndereco(){
+        Endereco endereco = new Endereco(1L,"Casa","teste",1,"Palmas");
+        return endereco;
+    }
+
+    public static EnderecoDTO createEnderecoDTO(){
+        return new EnderecoDTO(createEndereco(),1L);
+    }
+}
