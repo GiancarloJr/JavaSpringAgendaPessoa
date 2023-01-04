9019dae935e6548f20b4a23ed0990d4ec11daa16
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2023-01-04 00:06:53 -0300

    ResourceNotFoundException Custom Msg

---
+++ backend/src/main/java/spring/agenda/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package spring.agenda.service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
