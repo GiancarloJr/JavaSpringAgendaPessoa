6e022db16efba2e01ad7cab7fda44d22f1bf26ab
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2023-01-20 17:01:26 -0300

    Adicionando @EnableFeighClients para API VIA CEP

--- backend/src/main/java/spring/agenda/AgendapessoasApplication.java
+++ backend/src/main/java/spring/agenda/AgendapessoasApplication.java
@@ -2,8 +2,10 @@ package spring.agenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AgendapessoasApplication {
 
 	public static void main(String[] args) {
